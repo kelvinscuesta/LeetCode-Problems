6cca2f7f4a8c3ef1a6d75f9eb79be6b06c6ca58f
Kelvin Cuesta <kelvinscuesta@gmail.com>
2020-05-07 10:32:41 -0400

    Got solution, reminder to read carefully as I type and do multiple glance overs

---
+++ Easy/993cousinsBinaryTree.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function(root, x, y) {
+  // want to find each value's parents and depth levels
+  // then at the end compare them to see if they're equivalent
+  // if the parents are congruent we can return false
+  // if the depths are the same we can return true
+
+  let depthOfX;
+  let depthOfY;
+  let parentX;
+  let parentY;
+
+  // use to return the depth level and parent, want to only travel to parents
+  const dfs = (root, value, depth) => {
+    if (!root.left && !root.right) return;
+
+    let depthLeft;
+    let depthRight;
+
+    if (root.left) {
+      if (root.left.val === value) {
+        if (value === x) parentX = root;
+        if (value === y) parentY = root;
+        return depth + 1;
+      }
+      depthLeft = dfs(root.left, value, depth + 1);
+    }
+
+    if (root.right) {
+      // console.log('hit here')
+      if (root.right.val === value) {
+        if (value === x) parentX = root;
+        if (value === y) parentY = root;
+        return depth + 1;
+      }
+      depthRight = dfs(root.right, value, depth + 1);
+    }
+    // console.log(depthRight, depth);
+    // console.log(depthLeft)
+
+    // otherwise keep exploring
+
+    return depthLeft === undefined ? depthRight : depthLeft;
+  };
+  depthOfX = dfs(root, x, 0);
+  depthOfY = dfs(root, y, 0);
+
+  // console.log(depthOfY);
+
+  return depthOfX === depthOfY ? parentX !== parentY : false;
+};
